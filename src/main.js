e06579f659dd24813a263bda58f154ed314946cd
Jay Fincher <fincher_jay@yahoo.com>
2019-03-15 15:42:06 -0400

    refactoring the main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import Vue from 'vue'
 import App from './Main.vue'
-import cgMap from './components/cgMap.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
-
-new Vue({
-  el     : '#cgMap', 
-  components: { cgMap: cgMap },    
-  render : h => h('cgMap') // now we pass the name, so Vue will know it
-}).$mount('#cgMap')
